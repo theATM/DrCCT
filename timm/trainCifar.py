45c011d1e6de29aacf9181a04a4c03268be392a4
theATM <the_atm@wp.pl>
2023-04-19 22:04:22 +0200

    Update trainCifar.py - train on cifar

--- timm/trainCifar.py
+++ timm/trainCifar.py
@@ -92,6 +92,8 @@ group.add_argument('--train-split', metavar='NAME', default='train',
                    help='dataset train split (default: train)')
 group.add_argument('--val-split', metavar='NAME', default='validation',
                    help='dataset validation split (default: validation)')
+group.add_argument('--cifar', action='store_true', default=False,
+                   help='Use Cifar-100 dataset')
 group.add_argument('--dataset-download', action='store_true', default=False,
                    help='Allow download of dataset for torch/ and tfds/ datasets that support it.')
 group.add_argument('--class-map', default='', type=str, metavar='FILENAME',
