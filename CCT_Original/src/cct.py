50ad31d47dd4df27ceafac4db2b90e63a1db14c7
froxec <picmego@gmail.com>
2023-04-27 21:05:13 +0200

    added cct hybrid

--- CCT_Original/src/cct.py
+++ CCT_Original/src/cct.py
@@ -448,3 +448,13 @@ def cct_7_3x1_32_c100_hybrid(pretrained=False, progress=False,
                  img_size=img_size, positional_embedding=positional_embedding,
                  num_classes=num_classes,
                  *args, **kwargs)
+
+@register_model
+def cct_2_3x2_32_c100(pretrained=False, progress=False,
+                 img_size=32, positional_embedding='learnable', num_classes=100,
+                 *args, **kwargs):
+    return cct_2('cct_2_3x2_32', pretrained, progress,
+                 kernel_size=3, n_conv_layers=2,
+                 img_size=img_size, positional_embedding=positional_embedding,
+                 num_classes=num_classes,
+                 *args, **kwargs)
